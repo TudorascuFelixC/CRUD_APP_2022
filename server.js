83f7d24c1ec13098154a16e7c75fed980810c0cd
Felix Tudorascu <82870321+TudorascuFelixC@users.noreply.github.com>
2022-06-09 11:46:04 +0100

    11:45 9/6

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
 const path = require('path');
-const uri = process.env.MONGO_URI;
 
 const connectDB = require('./server/database/connection');
 const { options } = require('nodemon/lib/config');
@@ -11,27 +10,16 @@ const { options } = require('nodemon/lib/config');
 const app = express();
 
 dotenv.config({path:'./config.env'})
-//TEST
-var http = require('http');
 
-http.createServer(function(request, response){
-
-    //The following code will print out the incoming request text
-    request.pipe(response);
-
-}).listen(8080, '127.0.0.1');
-
-console.log('Listening on port 8080...');
-//if we work on a local host the port will be 8080
 const PORT = process.env.PORT|| 8080
 
 //log requests
 app.use(morgan('tiny'));
 
-//This is connection between mongodb connection and our server
+//mongodb connection
 connectDB();
 
-//Parse request to body-parser
+//parse request to body-parser
 app.use(bodyparser.urlencoded({extended:true}))
 
 //set view engine
@@ -44,7 +32,7 @@ app.use('/img', express.static(path.resolve(__dirname,"assets/img")))
 app.use('/js', express.static(path.resolve(__dirname,"assets/js")))
 
 
-//This command will load routers from server/routes/router.js
+//load routers
 app.use('/', require('./server/routes/router'))
 
-app.listen(PORT, ()=>{console.log('Server is running on http://localhost:${8080}/')});
+app.listen(PORT, ()=>{console.log('Server is running on http://localhost:${3000}')});
